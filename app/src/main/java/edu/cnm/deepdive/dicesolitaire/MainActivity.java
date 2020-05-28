0143a67a692a246ca328e88430eb316257dd8c3a
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-05-28 17:05:29 -0600

    Created references on android

--- app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
@@ -1,13 +1,41 @@
 package edu.cnm.deepdive.dicesolitaire;
 
+import android.content.res.Resources;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import edu.cnm.deepdive.dicesolitaire.modlel.Roll;
+import java.text.NumberFormat;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+private static final String LABEl_ID_FORMAT = "pair_%d_label";
+  private static final String COUNT_ID_FORMAT = "pair_%d_count";
 
+  private int minPairValue = 2;
+  private int maxPairValue;
+  private TextView[] labels;
+  private ProgressBar[] count;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    maxPairValue = 2 * Roll.NUM_FACES;
+    labels = new TextView[maxPairValue - minPairValue + 1];
+    count = new ProgressBar[maxPairValue - minPairValue + 1];
+    Resources res = getResources();
+    Random rng = new Random();
+    NumberFormat formatter = NumberFormat.getNumberInstance();
+    for (int i = minPairValue; i <= maxPairValue; i++) {
+      String labelISting = String.format(LABEl_ID_FORMAT, i);
+      int labelId = res.getIdentifier(labelISting, "id", getPackageName());
+      labels[i- minPairValue] = findViewById(labelId);
+      labels[i - minPairValue].setText(formatter.format(i));
+      String countIdString = String.format(COUNT_ID_FORMAT, i);
+      int countId = res.getIdentifier(countIdString, "id" , getPackageName());
+      count[i - minPairValue] = findViewById(countId);
+      count[i - minPairValue].setProgress(1 + rng.nextInt(10));
+    }
   }
 }
--- app/src/main/java/edu/cnm/deepdive/dicesolitaire/modlel/Roll.java
+++ app/src/main/java/edu/cnm/deepdive/dicesolitaire/modlel/Roll.java
@@ -6,14 +6,14 @@ import java.util.Random;
 public class Roll {
 
   public static final int NUM_DICE = 5;
-      public static final int NUM_FACE = 6;
+      public static final int NUM_FACES = 6;
 
-      private final int[] dice;
+  private final int[] dice;
 
       public Roll(Random rng) {
         dice = new int[NUM_DICE];
         for (int i = 0; i < NUM_DICE; i++){
-          dice[i] = 1 + rng.nextInt(NUM_FACE);
+          dice[i] = 1 + rng.nextInt(NUM_FACES);
         }
         Arrays.sort(dice);
       }
